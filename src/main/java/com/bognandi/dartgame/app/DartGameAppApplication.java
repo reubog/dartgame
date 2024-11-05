bb8337f78f008e99e9e81e5a086fc622cd880e58
reuben bognandi <reuben.bognandi@softhouse.se>
2024-11-05 07:21:39 +0100

    remove spring boot

--- src/main/java/com/bognandi/dartgame/app/DartGameAppApplication.java
+++ src/main/java/com/bognandi/dartgame/app/DartGameAppApplication.java
@@ -1,8 +1,6 @@
 package com.bognandi.dartgame.app;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class DartGameAppApplication {
 	public static void main(String[] args) {
 		JavaFxApplication.launch(args);
--- src/main/java/com/bognandi/dartgame/app/JavaFxApplication.java
+++ src/main/java/com/bognandi/dartgame/app/JavaFxApplication.java
@@ -6,21 +6,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
 
 public class JavaFxApplication extends Application {
-    private ConfigurableApplicationContext applicationContext;
-
-    @Override
-    public void init() {
-        applicationContext = new SpringApplicationBuilder(DartGameAppApplication.class).run();
-    }
 
     @Override
     public void start(Stage stage) throws Exception {
         // Load the FXML file and create the scene
-        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"), null, null, applicationContext::getBean);
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
         Scene scene = new Scene(root, 800, 600);
         stage.setScene(scene);
         stage.show();
@@ -28,11 +20,6 @@ public class JavaFxApplication extends Application {
 
     @Override
     public void stop() {
-        applicationContext.close();
         Platform.exit();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
